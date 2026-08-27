2ca74f1c642482e68aa945b1183c0709b8c91db3
enchevdev <enchev.dev@gmail.com>
2026-08-27 13:25:31 +0200

    Initialize weather app

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("WEATHER APP");
+
+            Console.WriteLine("Press Enter To Continue...");
+
+            Console.ReadLine();
         }
     }
 }
