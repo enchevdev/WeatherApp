060f4396a813992f2a1f968b0dc40ddd78f23be7
enchevdev <enchev.dev@gmail.com>
2026-08-28 12:06:20 +0200

    Add Location model

---
+++ Location.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    internal class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        
+
+
+
+        public Location(decimal latitude, decimal longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+    }
+}
