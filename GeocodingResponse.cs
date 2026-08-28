c7e4838e1c24b3ef33d6ffe1642ee1364d4d3c97
enchevdev <enchev.dev@gmail.com>
2026-08-28 12:12:30 +0200

    Add geocoding response mapping

---
+++ GeocodingResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    internal class GeocodingResponse
+    {
+        public List<Location> Results { get; set; }
+
+    }
+}
